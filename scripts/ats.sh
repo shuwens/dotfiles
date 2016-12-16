9c8d13560cb32b2405858dd541f4475bb603b23a
shwsun <shwsun@bu.edu>
2016-12-16 09:59:37 -0500

    various update

---
+++ scripts/ats.sh
@@ -0,0 +1,3 @@
+wget http://ats-lang.github.io/SCRIPT/C9-ATS2-install-cs520.sh
+
+sh -v C9-ATS2-install-cs520.sh
---
+++ scripts/vim_lua.sh
@@ -0,0 +1,14 @@
+cd ~
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-pythoninterp=yes \
+            --with-python-config-dir=/usr/lib/python2.7/config \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=/usr/lib/python3.5/config \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr
+make VIMRUNTIMEDIR=/usr/share/vim/vim80
