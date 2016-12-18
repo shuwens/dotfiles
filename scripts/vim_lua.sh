f770245a35deff9a73f746518af1d2040e12e6a5
shwsun <shwsun@bu.edu>
2016-12-18 16:20:32 -0500

    update vim_lua isntall script

--- scripts/vim_lua.sh
+++ scripts/vim_lua.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------
+# @brief: simple shell script to build up-to-data vim on ubuntu 16.04
+# @author: Shuwen Jethro Sun
+#
+# @copy: basic part is from YouCompleteMe wiki page
+#-------------------------------------------------------------------------
+
+sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
+
+sudo apt-get remove vim vim-runtime gvim
+
 cd ~
 git clone https://github.com/vim/vim.git
 cd vim
@@ -12,3 +28,7 @@ cd vim
             --enable-luainterp=yes \
             --enable-gui=gtk2 --enable-cscope --prefix=/usr
 make VIMRUNTIMEDIR=/usr/share/vim/vim80
+
+sudo apt-get install checkinstall
+cd ~/vim
+sudo checkinstall
