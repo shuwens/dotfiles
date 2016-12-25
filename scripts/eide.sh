9c11421bac31b067e9a4a170bd42dc218a41e598
shwsun <shwsun@bu.edu>
2016-12-24 22:54:10 -0500

    emacs update

---
+++ scripts/eide.sh
@@ -0,0 +1,8 @@
+rm -rf ~/.emacs.d
+#git clone git@github.com:shwsun/emacs.d.git  ~/.emacs.d
+
+#---------------------------------------------------
+git clone -b beta --single-branch git@github.com:shwsun/emacs.d.git ~/.emacs.d
+
+
+#emacs
---
+++ scripts/emacs_25.sh
@@ -0,0 +1,6 @@
+sudo add-apt-repository -y ppa:ubuntu-elisp
+sudo apt-get update
+sudo apt-get install emacs-snapshoti
+
+
+
