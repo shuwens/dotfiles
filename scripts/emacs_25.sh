4742151e8588413b44bd682b256cc3ed915d52a0
Shuwen (Jethro) Sun <shwsun@bu.edu>
2017-02-23 02:27:47 -0500

    update irssi

--- scripts/emacs_25.sh
+++ scripts/emacs_25.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 sudo add-apt-repository -y ppa:ubuntu-elisp
 sudo apt-get update
-sudo apt-get install emacs-snapshoti
+sudo apt-get install emacs-snapshot
 
-
-
+# end of [emacs_25.sh]
