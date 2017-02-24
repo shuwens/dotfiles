6dda142c51ed90242f9d1919955c4646d815f9f4
Shuwen (Jethro) Sun <shwsun@bu.edu>
2017-02-24 15:36:35 -0500

    small updates

--- scripts/eide.sh
+++ scripts/eide.sh
@@ -3,5 +3,6 @@ rm -rf ~/.emacs.d
 
 #---------------------------------------------------
 git clone -b beta --single-branch git@github.com:shwsun/emacs.d.git ~/.emacs.d
+cd ~/git && git clone https://github.com/alexott/ecb
 
 #emacs
