5932e2fd2b2ee4dc44a4daac5c559faac976cafd
Shuwen (Jethro) Sun <shwsun@bu.edu>
2017-04-29 00:17:28 -0400

    Fingers crossed!

--- scripts/eide.sh
+++ scripts/eide.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+
 rm -rf ~/.emacs.d
-#git clone git@github.com:shwsun/emacs.d.git  ~/.emacs.d
+#git clone https://github.com/aclements/.emacs.d.git ~/.emacs.d
 
 #---------------------------------------------------
 git clone -b beta --single-branch git@github.com:shwsun/emacs.d.git ~/.emacs.d
-cd ~/git && git clone https://github.com/alexott/ecb
 
 #emacs
---
+++ scripts/ide.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf ~/.vim* && git clone git@github.com:shwsun/yavc.git ~/.vim && ln -s ~/.vim/vimrc ~/.vimrc 
+
+#rm -rf ~/.vim* && git clone git@github.com:shwsun/vim-notetaking.git ~/.vim && ln -s ~/.vim/vimrc ~/.vimrc && ln -s ~/.vim/vimpcrc ~/.vimpcrc
+
+#rm -rf ~/.vim* ~/.oh-my-vim && curl -L https://raw.github.com/liangxianzhe/oh-my-vim/master/tools/install.sh | sh
+
+
+# end of vide.sh
