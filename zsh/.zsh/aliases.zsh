4015d1bb8ae5712e594af5d8d25d5f1cfbe536d5
shwsun <shwsun@bu.edu>
2017-10-15 15:13:24 -0400

    checkpoint: changed some files of emacs aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -121,7 +121,10 @@ alias jn="jupyter notebook"
 alias F="thunar ."
 alias cls="clear"
 alias vi="vim"
-alias ee="emacsclient"
+#alias ee="emacsclient"
+alias ee="emacsclient -t"                      # used to be "emacs -nw"
+alias semac="sudo emacsclient -t"                # used to be "sudo emacs -nw"
+alias EE="emacsclient -c -a emacs"           # new - opens the GUI with alternate non-daemon
 
 # grep and find
 #alias g="grep --color=autp -ri"
