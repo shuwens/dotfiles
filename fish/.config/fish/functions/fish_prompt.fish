ddde85bb33b9d8ebc60e03d9d621b82a85d79a4d
Jethro Shuwen Sun <sun.shuw@northeastern.edu>
2018-09-20 15:03:38 -0400

    Checkpoint: prompt change to fancy one

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,24 +1,4 @@
 function fish_prompt
-  set_color white
-  echo -n "["(date "+%H:%M")"] "
-  set_color yellow
-  echo -n (whoami)
-  set_color normal
-  echo -n '@'
-  set_color blue
-  echo -n (hostname)" "
-  set_color green
-  echo -n (prompt_pwd | sed "s_$HOME""_~_")""
-  set_color brown
-  printf '%s ' (__fish_git_prompt)
-  set_color red
-  if [ (whoami) = "root" ]
-    echo -n '# '
-  else
-    echo -n '$ '
-  end
-  if set -q VIRTUAL_ENV
-      echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-  end
-  set_color normal
+    powerline-shell --shell bare $status
 end
+
