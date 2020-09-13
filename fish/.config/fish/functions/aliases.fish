f6052f36ab26ad96300e63827a0a3a2509928ed0
Shuwen Jethro Sun <jethro.rules@real.com>
2020-09-13 16:03:26 -0400

    If it's stupid and it works, it ain't stupid

--- fish/.config/fish/functions/aliases.fish
+++ fish/.config/fish/functions/aliases.fish
@@ -10,22 +10,15 @@
 abbr -a -U vimdiff nvim -d
 abbr -a -U sduo sudo
 abbr -a -U upssh "vagrant up; and vagrant ssh"
-abbr -a -U b brew
+abbr -a -U vi vim
 abbr -a -U vim nvim
-abbr -a -U v vagrant
 abbr -a -U e nvim
 abbr -a -U ws "wormhole send"
 abbr -a -U wr "wormhole receive"
-abbr -a -U vi vim
-# abbr -a -U jn jupyter notebook
 abbr -a -U R ranger
-abbr -a -U lab "GIT_SSH_COMMAND=\"ssh -i ~/.ssh/jethrosun_rsa\" git $argv"
-abbr -a amz 'env AWS_SECRET_ACCESS_KEY=(pass www/aws-secret-key | head -n1)'
-abbr -a ais "aws ec2 describe-instances | jq '.Reservations[] | .Instances[] | {iid: .InstanceId, type: .InstanceType, key:.KeyName, state:.State.Name, host:.PublicDnsName}'"
-
 abbr -a bs bibtex-search
 
-abbr -a py3 "pyenv activate py3"
+abbr -a py "pyenv activate py3"
 abbr -a pyn "pyenv activate newest"
 
 abbr -a note 'nvim ~/notes/talks.org'
@@ -35,15 +28,12 @@ abbr -a -U c cargo
 abbr -a -U cargot cargo t
 abbr -a -U clippy cargo clippy
 abbr -a -U cl cargo clippy
-#abbr -a -U r rustup
 abbr -a -U ra rustup add component
 
 # git
 abbr -a -U g git
 abbr -a -U gm git m
-abbr -a -U m 'git commit; and git push'
 abbr -a -U gc git clone
-abbr -a -U ggo git checkout
 abbr -a -U gb git branch
 abbr -a -U gd git diff
 abbr -a -U gr git rebase -i HEAD~5
@@ -65,7 +55,6 @@ abbr -a -U Ev 'nvim ~/.config/nvim/init.vim'
 abbr -a -U Es 'nvim ~/.config/nvim/spell/en.utf-8.add'
 abbr -a -U Ee 'nvim ~/.spacemacs'
 abbr -a -U Et 'nvim ~/.tmux.conf'
-abbr -a -U Ex 'nvim ~/.xmonad/xmonad.hs'
 
 abbr -a -U df 'command df -m'
 #abbr -a -U su 'command su -m'
@@ -78,22 +67,12 @@ function UpdateFile -a filename
     scp $filename nu-ccis:~/.www/tmp/
 end
 
-abbr -a -U ggco 'git switch'
-abbr -a -U fl 'clear; and flow-limit'
 
 # abbr -a -U jn "jupyter notebook --browser=firefox"
 abbr -a -U jn 'jupyter notebook --browser="firefox-developer-edition %s"'
 
 abbr -a -U vim-norc 'vim -u NORC'
 abbr -a -U vim-none 'vim -u NONE'
-
-#set LS_COLORS dxfxcxdxbxegedabagacad
-function lsd -d 'List only directories (in the current dir)'
-    command ls -d */ | sed -Ee 's,/+$,,'
-end
-
-# Colorized cat (will guess file type based on contents)
-abbr -a -U ccat 'pygmentize -g'
 
 abbr -a -U c "cargo"
 abbr -a -U r "cargo r"
@@ -120,8 +99,6 @@ function take
     set -l dir $argv[1]
     mkdir -p $dir; and cd $dir
 end
-abbr -a -U cx 'chmod +x'
-abbr -a -U 'c-x' 'chmod -x'
 
 ## My own stuff!
 abbr -a -U gs "git status"
@@ -133,9 +110,7 @@ abbr -a -U ,, "make clean"
 abbr -a -U vi "nvim"
 abbr -a -U :q "sudo reboot"
 
-function bar
-    eval "sudo -E -u jethros $HOME/.config/polybar/launch.sh &"
-end
+abbr -a -U vpn "eval /etc/profile.d/PanMSInit.sh; sleep 5; globalprotect connect -portal vpn.northeastern.edu"
 
 # wget -A pdf,mp3,ps,djvu,tex,doc,docx,xls,xlsx,gz,ppt,mp4,avi,zip,rar,html,htm,asp,php --no-clobber --convert-links --random-wait -r -p -E -e robots=off -U mozilla https://pg.ucsd.edu
 abbr -a -U WgetScrape "wget -A pdf -m -p -E -k -K -np"
@@ -233,17 +208,6 @@ function md2pdf
     end
 end
 
-function lpmd
-    set infile $argv[1]
-    set --erase argv[1]
-    md2pdf $infile - | lp $argv -
-end
-
-function pdfo
-    echo $argv | xargs pdflatex
-    echo $argv | sed 's/\.tex$/.pdf/' | xargs xdg-open
-end
-
 function px
     ssh -fND localhost:8080 -C jon@ssh.thesquareplanet.com -p 222
 end
@@ -298,19 +262,9 @@ end
 # ----------------------------------
 # Compiling stuff
 # ----------------------------------
-abbr -a -U ghcd "ghc -dynamic"
-abbr -a -U g11 'g++ -std=c++11 -O2'
-abbr -a -U g+ "g++ -std=gnu++11 -Wall -Wextra -g"
 
 # Convenience abbr -a -Ues
 abbr -a -U run 'sudo systemctl start'
 abbr -a -U restart 'sudo systemctl restart'
 abbr -a -U stop 'sudo systemctl stop'
-# ---------------------------------
-abbr -a -U emacs 'emacs -nw'
 
-## DEPRECATED
-#abbr -a -U UpdateResume "scp ~/writing/phd-application/nice_cv/sun_cv.pdf shwsun@csa2.bu.edu:~/public_html/tmp"
-#abbr -a -U UpdateStatement "scp ~/writing/phd-application/sop/statement.pdf shwsun@csa2.bu.edu:~/public_html/tmp"
-
-#abbr -a -U jn "jupyter notebook --browser=google-chromium-browser"
