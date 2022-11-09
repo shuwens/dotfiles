7c97ccf81fbccbe433f8efa5ace0e2fa834f0798
Shuwen Sun <sun.shuw@northeastern.edu>
2022-11-08 23:11:52 -0500

    fix

--- scripts/.scripts/remarkable.sh
+++ scripts/.scripts/remarkable.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # set -x
 # /home/jethros/dev/achtung/survey/src
-cp ~/dev/achtung/survey/src/paper.pdf ~/Downloads/remarkable/pvn/paper_$(date +"%Y-%m-%d--%H:%M").pdf
+# cp ~/dev/achtung/survey/src/paper.pdf ~/Downloads/remarkable/pvn/paper_$(date +"%Y-%m-%d--%H:%M").pdf
 
-cp ~/dev/achtung/audit/paper.pdf ~/Downloads/remarkable/pvn2/paper_$(date +"%Y-%m-%d--%H:%M").pdf
+# cp ~/dev/achtung/audit/paper.pdf ~/Downloads/remarkable/pvn2/paper_$(date +"%Y-%m-%d--%H:%M").pdf
 
-cd ~/Downloads/remarkable/pvn
-echo -e "mput pvn1" | rmapi
+# cd ~/Downloads/remarkable/pvn
+# echo -e "mput pvn1" | rmapi
+#
+# cd ~/Downloads/remarkable/pvn2
+# echo -e "mput pvn2" | rmapi
+#
+# cd ~/Downloads/remarkable/tr
+# echo -e "mput ToRead" | rmapi
 
-cd ~/Downloads/remarkable/pvn2
-echo -e "mput pvn2" | rmapi
+cd ~/Downloads/remarkable/interview
+echo -e "mput CodingInterview" | rmapi
 
-cd ~/Downloads/remarkable/tr
-echo -e "mput ToRead" | rmapi
+
 
