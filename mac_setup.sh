0337069dcde923ad2c9a2887759517d110f777ba
Shuwen Sun <sun.shuw@northeastern.edu>
2024-03-20 10:54:57 -0400

    foo

--- mac_setup.sh
+++ mac_setup.sh
@@ -6,7 +6,8 @@ stow bin         -t ~
 stow scripts	 -t ~
 stow mac         -t ~
 stow zk		  -t ~
-stow -t ~   spacemacs
+stow doom   -t ~
+# stow -t ~   spacemacs
 
 cd config || exit
 stow -t ~/.config  .config
