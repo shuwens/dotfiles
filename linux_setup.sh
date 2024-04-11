e144fee549437ff33dab698d4fce3d628cc39b5a
Shuwen Sun <sun.shuw@northeastern.edu>
2024-04-10 20:44:32 -0400

    Working on WIP

--- linux_setup.sh
+++ linux_setup.sh
@@ -25,7 +25,7 @@ arch)
   sudo pacman -S firefox-developer-edition emacs tmux alacritty neovim the_silver_searcher tk zathura zathura-pdf-poppler tailscale ranger
   sudo pacman -S ttf-jetbrains-mono-nerd ttf-fira-code ttf-fira-sans ttf-linux-libertine ttf-dejavu-sans-mono-powerline-git
 
-  sudo paru -S google-chrome slack-desktop dropbox ttf-monaco 
+  sudo paru -S google-chrome slack-desktop dropbox ttf-monaco
   # sudo paru -S texlive biber texlive-lang
   ;;
 
@@ -35,7 +35,3 @@ centos) echo "This is CentOS!"
 *) echo "This is an unknown distribution."
   ;;
 esac
-
-## dot config
-#cd config || exit
-#stow -t ~/.config  .config
